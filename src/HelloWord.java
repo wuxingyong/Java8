2d36f23c844d473c9cc41011cf162e678fb17f98
wuxingyong <303775491@qq.com>
2019-04-03 13:53:20 +0800

    Update HelloWord.java

--- src/HelloWord.java
+++ src/HelloWord.java
@@ -2,6 +2,6 @@ package src;
 
 public class HelloWord {
     public static void main(String[] args) {
-        System.out.println("Hello Word!");
+        System.out.println("Hello Word! Wuxingyong!");
     }
 }
